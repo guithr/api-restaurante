d31ba3972e3f75c2320d8baa03a76f23b1982494
guithr <guilherme.thrmatos@gmail.com>
2025-02-11 10:38:09 -0300

    feat: organize project structure with routes and controllers

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import exp from "constants";
+import { routes } from "./routes";
 import express from "express";
 
 const PORT = 3333;
 
 const app = express();
 app.use(express.json());
+
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
