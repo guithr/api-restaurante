e55355bbebbb7260524ea20e1d3cfeb90eaf2dd9
guithr <guilherme.thrmatos@gmail.com>
2025-02-11 12:00:38 -0300

    feat: add Zod for request data validation

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
 import { AppError } from "@/utils/AppError";
 
 export function errorHandling(
@@ -9,6 +10,12 @@ export function errorHandling(
 ) {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({ message: error.message });
+  }
+
+  if (error instanceof ZodError) {
+    return response
+      .status(400)
+      .json({ message: "Validation error", issues: error.format() });
   }
 
   return response.status(500).json({ message: error.message });
